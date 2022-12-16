9e0f7d350393b0f1ab94bbc0e81d88aee8e2ab82
nisim08 <nisimeitan08@gmail.com>
2022-12-16 22:31:09 +0200

    you can see

--- js3.js
+++ js3.js
@@ -7,7 +7,10 @@ window.location = "index.html"
 document.getElementById("b2").onclick = function() {
 
     window.location = "https://youtu.be/RAkSkMRwPYs?t=39"
+}
+
+document.getElementById("scrate").onclick = function() {
+
+    window.location = "https://nisim08.github.io/web2/"
     
     }
-    
-
